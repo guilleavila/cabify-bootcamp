eb6f6a96a371cfc78a27a6703e5e72ac68d61d7b
Guille Ávila <guille9797@hotmail.com>
2022-06-01 14:14:02 +0200

    changed gitignore

--- messageapp/services/APIHandler.js
+++ messageapp/services/APIHandler.js
@@ -4,14 +4,10 @@ module.exports = class ApiHandler {
     constructor() {
         this.axiosApp = axios.create({
             baseURL: 'http://messageapp:3000',
-            headers: {
-                'Content-Type': 'application/json;charset=UTF-8'
-            }
         })
     }
 
     sendMessage(messageInfo) {
-        console.log('El message ---->', messageInfo)
         return this.axiosApp.post('/message', messageInfo)
     }
 }
