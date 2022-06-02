9c48054b4a3981cee4c131ca2bed62dd88b08214
Guille Ávila <guille9797@hotmail.com>
2022-06-02 11:45:43 +0200

    untrack DS_Store

--- messageapp/routes/index.routes.js
+++ messageapp/routes/index.routes.js
@@ -8,16 +8,16 @@ router.post("/messages", (req, res, next) => {
   const { destination, message } = req.body
 
   if (!destination && !message) {
-    res.status(400).json({ message: "You should not pass empty object" })
+    res.status(422).json({ message: "You should not pass empty object" })
   }
   else if (!destination) {
-    res.status(400).json({ message: "Destination is required" })
+    res.status(422).json({ message: "Destination is required" })
   }
   else if (!message) {
-    res.status(400).json({ message: "Message is required" })
+    res.status(422).json({ message: "Message is required" })
   }
   else if (typeof destination !== "string" || typeof message !== "string") {
-    res.status(400).json({ message: "Only strings allowed" })
+    res.status(422).json({ message: "Only strings allowed" })
   }
   else {
     messageHandler
